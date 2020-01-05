e0393b3430530cc4f2f56d564f2d92651830930e
Denis Yusupov <56558758+opalmolive@users.noreply.github.com>
2020-01-06 01:55:06 +0300

    Update lab1.sh

--- bash/lab1.sh
+++ bash/lab1.sh
@@ -1,2 +1,24 @@
-#!/bin/bash
-echo 222
+#!/bin/vcbash
+echo "filename; type; size; date; duration">./result.xls
+#создание функции
+files()
+{
+  (
+  for file in "$1"/*
+  do
+    ext=$(echo ${file##*.})
+    path=$file
+    name=$(echo "$path"|rev|cut -f1 -d /|rev)
+    
+    if [[$ext=="mkv" || $ext =="mp4" || $ext =="avi" || $ext=="mp3"]]; then
+        echo $name,$ext,$(stat --printf="%s,%z" "$path"),$(mediainfo "$path"|head -n6|tail -n1|cut -c44-)
+    elif [[$name =="*"]]; then
+        continue
+    elif [[-d "$path" && $(echo "$path"|wc -l) -ne 0]]; then
+        files "$path"
+    else
+        echo $name, $ext,$(stat --printf="%s,%z" "$path")
+    fi
+  done)>>./result.xls
+}
+files .
