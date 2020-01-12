cce6c3f7fcc5ce918e9ca5f5267ac4f17e120586
Denis Yusupov <56558758+opalmolive@users.noreply.github.com>
2020-01-12 04:14:15 +0300

    Update lab2.sh

--- bash/lab2.sh
+++ bash/lab2.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+mkdir -p ws/src && cd ws/src
+git clone https://github.com/airalab/robonomics_comm
+catkin_init_workspace && cd .. && catkin_make
+wget http://bootstrap.aira.life/robonomics.sh
+chmod +x ./robonomics.sh
+./robonomics.sh init
 
